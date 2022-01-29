71f5a4b83f760048d08c56bb26731c294f06bf7e
ohdowon064 <dhehdnjs03@gmail.com>
2022-01-29 23:59:43 +0900

    Page struct 1. 페이지 제목과 내용을 저장한다. 2. save 메서드로 제목.txt의 파일을 저장한다.

--- src/wiki.go
+++ src/wiki.go
@@ -1,5 +1,7 @@
 package main
 
+import "os"
+
 /*
 	1. 메서드 전달 및 저장을 위한 data structure
 	2. net/http 패키지 사용하여 웹어플리케이션 구축
@@ -7,3 +9,14 @@ package main
 	4. regexp 패키지로 사용자 input validate
 	5. closure 사용
 */
+
+type Page struct {
+	Title string
+	Body  []byte // body에는 이후 사용될 io 라이브러리때문에 []byte 사용
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+	// os.WriteFile: byte slice를 파일로 저장하는 표준 라이브러리
+}
