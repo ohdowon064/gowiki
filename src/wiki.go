27d0018b6c788d4366e2cf082838f1cedd836342
ohdowon064 <dhehdnjs03@gmail.com>
2022-01-29 23:52:55 +0900

    rename main.go to wiki.go

---
+++ src/wiki.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+	1. 메서드 전달 및 저장을 위한 data structure
+	2. net/http 패키지 사용하여 웹어플리케이션 구축
+	3. html/template 패키지를 사용하여 HTML 템플릿 생성
+	4. regexp 패키지로 사용자 input validate
+	5. closure 사용
+*/
