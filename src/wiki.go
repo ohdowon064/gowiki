3f0ce24af70240cfcd364069a1b855d82c0f53ca
ohdowon064 <dhehdnjs03@gmail.com>
2022-02-03 23:58:14 +0900

    loadPage 함수의 error 핸들링 - nil여부로 에러 판단 후 핸들링가능 - nil이면 성공

--- src/wiki.go
+++ src/wiki.go
@@ -28,8 +28,30 @@ func (p *Page) save() error {
 	// os.WriteFile: byte slice를 파일로 저장하는 표준 라이브러리
 }
 
-func loadPage(title string) *Page {
+/*
+	- 파일이름(title)을 파라미터로 받아 파일 내용을 읽어온다.
+	- 읽어온 내용을 새로운 body변수에 넣고 Page 구조체 생성
+	- 파리미터로 받은 title, 파일을 읽어서 만든 body로 생성된 Page 리터럴 포인터를 반환
+		- 리터럴은 값 그자체를 의미한다.
+
+	- 리터럴이란?
+		- 소스코드의 고정된 값을 대표하는 용어, 문자 그대로의 값
+		page = Page{Title: title, Body: body}
+		return &page
+		- 이렇게 했으면 Page 변수 포인터를 반환하는 것이지만
+		- 바로 return &Page{} 이렇게 했기 때문에 Page 리터럴 포인터를 반환한 것이다.
+
+	- 표준 라이브러리 os.ReadFile 함수는 []byte, error를 반환한다.
+	- _는 blank identifier를 의미한다. -> 안쓴다는 뜻, 아무것에도 할당하지않는다.
+	- error를 함께 반환하여 에러 핸들링을 한다.
+		- 두번째 반환값(error)을 체크한다.
+		- nil인 경우 성공, nil 아니면 에러발생으로 핸들링 가능
+*/
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, _ := os.ReadFile(filename)
-	return &Page{Title: title, Body: body}
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
